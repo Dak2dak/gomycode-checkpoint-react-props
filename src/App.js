c279a73698ffc6267c1a0b58708442f1749d2c2c
Didier Dakoury <dazibao76@gmail.com>
2021-11-11 13:20:02 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ProfileComponent from './profile/ProfileComponent.js';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/profile/ProfileComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const ProfileComponent = ({fullName, bio, profession}) =>{
+    return(
+        
+    );
+}
+
+export default ProfileComponent;
