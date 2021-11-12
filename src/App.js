784c511e31b6f43d25b893211e9afc13824bad75
Didier Dakoury <dazibao76@gmail.com>
2021-11-12 15:43:37 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import ProfileComponent from './profile/ProfileComponent.js';
 function App() {
   return (
     <div className="App">
-      
+
+      {/* passing fullName, bio, profession as props */}
+      <ProfileComponent 
+        fullName='Didier Dakoury' 
+        bio='I am easy to get along with, very kind and hardworking.' 
+        profession='I am a front-end developer'
+      />
+
+
+      {/* passing image as children props */}
+      <ProfileComponent></ProfileComponent>
     </div>
   );
 }
--- src/profile/ProfileComponent.js
+++ src/profile/ProfileComponent.js
@@ -1,9 +1,24 @@
 import React from "react";
 
 
-const ProfileComponent = ({fullName, bio, profession}) =>{
+const ProfileComponent = (props) =>{
+
+
     return(
-        
+        <div className="user-profile">
+            <div>
+                <img src={props.image}  alt="DidierDak.jpg"/>
+            </div>
+
+            <div className="user-info">
+                <h2>{props.fullName}</h2>
+                <div>
+                    <p><b>Bio: </b>{props.bio}</p>
+                    <p><b>Profession: </b>{props.profession}</p>
+                </div>
+            </div>
+            
+        </div>
     );
 }
 
