186eed443327ccafa57751b50ff61a75efd73e89
Didier Dakoury <dazibao76@gmail.com>
2021-11-18 09:05:19 +0000

    second commit of November 18th

--- src/profile/ProfileComponent.js
+++ src/profile/ProfileComponent.js
@@ -7,20 +7,22 @@ const ProfileComponent = (props) =>{
     <div onClick={() =>props.handleName(props.user.fullName)}
      style={{display: 'flex', justifyContent: 'center',
      border: '3px solid rgb(11, 150, 52)', borderBottomLeftRadius: '10',
-      borderTopLeftRadius: '10', fontSize: '20px', backgroundColor: 'rgb(243, 8, 8, 0.514)', gap: '1rem'}}>
-            <div className="bg-color">
-                <img src={props.user.img} alt="imageInSrc.jpg"/>
+      borderTopLeftRadius: '10', fontSize: '20px', backgroundColor: 'rgb(243, 8, 8, 0.514)',
+       gap: '1rem', width: '520px'}}>
+
+            <div style={{borderBottomLeftRadius: '8px', borderTopLeftRadius: '10px'}}>
+
+                <img src={props.user.img} style={{width: '150px', borderTopLeftRadius: '10px', borderBottomLeftRadius: '10px',
+                 borderBottomRightRadius: '10px'}} alt="imageInSrc.jpg"/>
             </div>
             <div>
                 <h2><b>Full name: </b>{props.user.fullName}</h2>
-                <br />
                 <div>
                     <p><b>Profession: </b>{props.user.profession}</p>
-                    <br />
                     <p><b>Bio: </b>{props.user.bio}</p>
                 </div>
             </div>
-        </div>
+    </div>
     );
 }
 
